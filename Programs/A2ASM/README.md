9260bd6ab033d4f06b6c3c67fa9e64d46ac6773b
BounceBruhni <114104512+BounceBruhni@users.noreply.github.com>
2026-04-29 00:51:51 +0200

    Applied gained info from teamwork

---
+++ Programs/A2ASM/README.md
@@ -0,0 +1,6 @@
+    ldr     R0,=VariableA   ; Anw01: Speicher-Referenz von VariableA wird in R0 gespeichert.
+    ldrb    R2,[R0]         ; Anw02: LSB, des Inhalts, welcher an der Speicheradresse R0 steht, wird in R2 gespeichert (0xef).
+    ldrb    R3,[R0,#1]      ; Anw03: 2tes Byte, welches an Adresse aus R0 steht, wird in R3 gespeichert. #1 = AnzahlVerschiebungen 
+    lsl     R2, #8          ; Anw04: Left-shift. Alle Bits um 8 Stellen nach links. Wenn Zielbit außerhalb von Speichbereich: beim LSB fortfahren.
+    orr     R2, R3          ; Anw05: xor zwischen Bits aus R2 und R3. Ergebnis in R2 abgelegt. 
+    strh    R2,[R0]         ; Anw06: 
--- Programs/A2ASM/Src/main.s
+++ Programs/A2ASM/Src/main.s
@@ -12,13 +12,14 @@
 
     EXPORT main         ; we need this for the linker - In this context it set the entry point,too
 
-ConstByteA  EQU 0xaffe ;setzt ConstByteA dem Wert 0xaffe gleich
+ConstByteA  EQU 0xaffe            ;0b 1010 1111 1111 1110
+                                ;ConstByteA == 0xaffe == true
     
 ;* We need some data to work on
     AREA DATA, DATA, align=2    
-VariableA   DCW 0xbeef ; word, reserves 32bit for VariableA
-VariableB   DCW 0x1234 ; word, reserves 32bit for VariableB
-VariableC DCW 0xaffe ;Hilfsvariable
+VariableA   DCW 0xbeef            ;0b 1011 1110 1110 1111
+VariableB   DCW 0x1234            ;0b 0001 0010 0011 0100
+VariableC    DCW 0x0000            ;reserve 2 words in memory
 
 ;* We need minimal memory setup of InRootSection placed in Code Section 
     AREA  |.text|, CODE, READONLY, ALIGN = 3    
@@ -26,28 +27,36 @@ VariableC DCW 0xaffe ;Hilfsvariable
 main
     BL initITSboard             ; needed by the board to setup
 ;* swap memory - Is there another, at least optimized approach?
-    ldr     R0,=VariableA   ; Anw01: lädt Speicheradresse von VariableA in R0, d.h. Referenz zu VariableA wird in R0 gespeichert.
-    ldrb    R2,[R0]         ; Anw02:  lädt ein Byte des Wertes (von "rechts", d.h. ab dem niedrigsten Stellenwert (Least Significant Byte LSB)), der an der in R0 gespeicherten Adresse zu finden ist, in R2.
-    ldrb    R3,[R0,#1]      ; Anw03: Das 2 Byte des Inhalts welcher in der Speicheradresse aus R0 steht, wird in R2 gespeichert. 
-                            ;       #1 gibt Anzahl der Verschiebungen um Größe der Einheit an(hier: 1 Byte)
-    lsl     R2, #8          ; Anw04: left-shift-operation wird durchgeführt. Alle Bits werden um #8 Stellen nach links verschoben. 
-                            ;        Falls Zielbit nicht in Speichergröße liegt, wird die Verschiebung an der Position des LSB fortgeführt. 
-                            ;        Es gilt: Zielposition = (Position + Verschiebung) mod speichergroeße
-    orr     R2, R3          ; Anw05  xor-Operation, die den Inhalt von R2 und R3 entsprechend vergleicht. Ergebnis wird in R2 gespeichert.
-    strh    R2,[R0]         ; Anw06 Speichert das least Significant halbwort des Wertes, welcher an der Speicheradresse aus R0 gespeichert ist, in R2.
+    ldr     R0,=VariableA   ; Anw01: 
+    ldrb    R2,[R0]         ; Anw02
+    ldrb    R3,[R0,#1]      ; Anw03
+    lsl     R2, #8          ; Anw04
+    orr     R2, R3          ; Anw05
+    strh    R2,[R0]         ; Anw06
     
 ;* const in var
-    mov     R5,#ConstByteA  ; Anw07: bewegt den hex-Wert für den ConstByteA äquivalent ist, in 
-    strh    R5,[R0]         ; Anw08: speichert r5 ein halbwort des Inhaltes, der Adresse aus R0, ab dem LSB.
-    
+    mov     R5,#ConstByteA  ; Anw07
+    strh    R5,[R0]         ; Anw08
+
+;* save 0xaffe in memory
+    ldr        R10,=VariableC    ; load memory address
+    strh    R5, [R10]        ; saves 0xfeaf in memory
+    ldrb    R11, [R10]
+    ldrb    R12, [R10,#1]
+    lsl        R11, #8
+    orr        R11, R12
+    strh    R11, [R10]
+
+
 ;* Change value from x1234 to x4321
     ldr     R1,=VariableB   ; Anw09
     ldrh    R6,[R1]         ; Anw0A
-    mov     R7, #0x30ED     ; Anw0B
+    mov     R7, #0x21de     ; Anw0B
     add     R6, R6, R7      ; Anw0C
     strh    R6,[R1]         ; Anw0D
     b .                     ; Anw0E
     
     ALIGN
+
     END
 
